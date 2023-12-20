8321b709401d572fb0c0989c8a8181c0afa5d5ef
Kirtan Modi <modikirtan19@gmail.com>
2023-12-19 22:05:48 -0600

    Update ResultsViewController and CalculatorViewController

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -10,25 +10,65 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
-    @IBOutlet weak var billTextField: UITextField!
-    
-    @IBOutlet weak var zeroPctButton: UIButton!
-    @IBOutlet weak var tenPctButton: UIButton!
-    @IBOutlet weak var twentyPctButton: UIButton!
-    @IBOutlet weak var splitNumberLabel: UILabel!
-    
-    
-    @IBAction func tipChanged(_ sender: UIButton) {
+  @IBOutlet weak var billTextField: UITextField!
+
+  @IBOutlet weak var zeroPctButton: UIButton!
+  @IBOutlet weak var tenPctButton: UIButton!
+  @IBOutlet weak var twentyPctButton: UIButton!
+  @IBOutlet weak var splitNumberLabel: UILabel!
+
+  var userSelectedTip: Double = 0.1
+  var userSelectedSplit: Int = 0
+    var finalResult = "0.0"
+
+  @IBAction func tipChanged(_ sender: UIButton) {
+      billTextField.endEditing(true)
+    if sender.currentTitle == "0%" {
+      zeroPctButton.isSelected = true
+      tenPctButton.isSelected = false
+      twentyPctButton.isSelected = false
+      userSelectedTip = 0.0
+    } else if sender.currentTitle == "10%" {
+      zeroPctButton.isSelected = false
+      tenPctButton.isSelected = true
+      twentyPctButton.isSelected = false
+      userSelectedTip = 0.1
+    } else {
+      zeroPctButton.isSelected = false
+      tenPctButton.isSelected = false
+      twentyPctButton.isSelected = true
+      userSelectedTip = 0.2
     }
+  }
+
+  @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    splitNumberLabel.text = String(format: "%.0f", sender.value)
+    userSelectedSplit = Int(sender.value)
+  }
+
+  @IBAction func calculatePressed(_ sender: UIButton) {
+    let bill = Double(billTextField.text!) ?? 0.0
+    let tip = bill * userSelectedTip
+    let total = bill + tip
+    let split = total / Double(userSelectedSplit)
+    let result = String(format: "%.2f", split)
+      finalResult = result
+    billTextField.endEditing(true)
+    print(result)
+    performSegue(withIdentifier: "goToResult", sender: self)
+  }
     
-    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultsViewController
+            
+            destinationVC.result = finalResult
+            destinationVC.tip = Int(userSelectedTip * 100)
+            destinationVC.split = userSelectedSplit
+        }
     }
-    
-    
-    @IBAction func calculatePressed(_ sender: UIButton) {
-    }
-    
-    override func viewDidLoad() {
+
+  override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
--- Tipsy/ResultsViewController.swift
+++ Tipsy/ResultsViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var settingsLabel: UILabel!
     
+    var result = "0.0"
+    var tip = 10
+    var split = 2
 
-    /*
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+      totalLabel.text = result
+      settingsLabel.text = "Split between \(split) people, with \(tip)% tip."
+
+  }
+
+  @IBAction func recalculatePressed(_ sender: UIButton) {
+      self.dismiss(animated: true, completion: nil)
+  }
+
+  /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
